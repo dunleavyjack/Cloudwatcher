4e052d2b240374f1d6e1c74ded8d1429e065335b
Jack Joseph <dunleavyjack@gmail.com>
2021-02-07 00:12:44 +0900

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import Header from './Header';
+
+
+const Home = (props) => {
+    const {
+        REACT_APP_SPOTIFY_CLIENT_ID,
+        REACT_APP_SPOTIFY_AUTHORIZE_URL,
+        REACT_APP_SPOTIFY_REDIRECT_URL
+    } = process.env;
+
+    const handleLogin = () => {
+        window.location = `${REACT_APP_SPOTIFY_AUTHORIZE_URL}?client_id=${REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${REACT_APP_SPOTIFY_REDIRECT_URL}&response_type=token&show_dialog=true`;
+    };
+
+    return (
+        <div className="login">
+            <Header />
+            <Button variant="into" type="submit" onClick={handleLogin}>
+                Login with Spotify
+            </Button>
+        </div>
+    )
+}
+
+export default connect()(Home)
